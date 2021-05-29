9fa1d1faab2732bb4cfb8b6a48edb579a9390faf
Danielle Puga <danielle.puga@gmail.com>
2021-05-29 13:28:08 -0700

    removed duplication with 'the' in line, changed line function to work with sentence start and verses

--- lib/house.rb
+++ lib/house.rb
@@ -2,7 +2,7 @@ class House
   
   attr_reader :sentence_start
 
-  def initialize(sentence_start="This is the")
+  def initialize(sentence_start="This is")
     @sentence_start = sentence_start
   end
 
@@ -21,39 +21,39 @@ class House
   # end
 
   def verses
-    @verses = ["malt that lay in", "rat that ate", "cat that killed", "dog that worried", "cow with the crumpled horn that tossed", "maiden all forlorn that milked", "man all tattered and town that kissed", "priest all shaven and sorn that married", "rooster that crowed in the morn that woke", "farmer sowing his corn that kept", "horse and the hound and the horn that belonged to"]
+    @verses = ["the malt that lay in", "the rat that ate", "the cat that killed", "the dog that worried", "the cow with the crumpled horn that tossed", "the maiden all forlorn that milked", "the man all tattered and torn that kissed", "the priest all shaven and shorn that married", "the rooster that crowed in the morn that woke", "the farmer sowing his corn that kept", "the horse and the hound and the horn that belonged to"]
   end
 
   def suffix
-    @suffix = "house that Jack built."
+    @suffix = "the house that Jack built.\n"
   end
 
   def line(number)
     case number
       when 1
-        "#{sentence_start} #{suffix}\n"
+        "#{sentence_start} #{suffix}"
       when 2
-        "#{sentence_start} #{verses[0]} the #{suffix}\n"
+        "#{sentence_start} #{verses[0]} #{suffix}"
       when 3
-        "#{sentence_start} #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[1]} #{verses[0]} #{suffix}"
       when 4
-        "#{sentence_start} cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 5
-        "#{sentence_start} dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 6
-        "#{sentence_start} cow with the crumpled horn that tossed the dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[4]} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 7
-        "#{sentence_start} maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[5]} #{verses[4]} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 8
-        "#{sentence_start} man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[6]} #{verses[5]} #{verses[4]} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 9
-        "#{sentence_start} priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[7]} #{verses[6]} #{verses[5]} #{verses[4]} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 10
-        "#{sentence_start} rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[8]} #{verses[7]} #{verses[6]} #{verses[5]} #{verses[4]} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 11
-        "#{sentence_start} farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[9]} #{verses[8]} #{verses[7]} #{verses[6]} #{verses[5]} #{verses[4]} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       when 12
-        "#{sentence_start} horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the #{verses[1]} the #{verses[0]} the house that Jack built.\n"
+        "#{sentence_start} #{verses[10]} #{verses[9]} #{verses[8]} #{verses[7]} #{verses[6]} #{verses[5]} #{verses[4]} #{verses[3]} #{verses[2]} #{verses[1]} #{verses[0]} #{suffix}"
       end
     end
 
